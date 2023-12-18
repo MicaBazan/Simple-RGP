c4383d80f90bbf221cd4e52060215fac1c100ba1
MicaBazan <micabazan5@gmail.com>
2023-12-17 22:18:46 -0300

    Creacion de variable y clase Player

---
+++ MiJuego.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace RPG
+{
+    partial class MiJuego
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblExperiencia = new System.Windows.Forms.Label();
+            this.lblNivel = new System.Windows.Forms.Label();
+            this.lblOro = new System.Windows.Forms.Label();
+            this.lblPuntosDeImpacto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Puntos de golpe:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Oro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Experiencia:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nivel:";
+            // 
+            // lblExperiencia
+            // 
+            this.lblExperiencia.AutoSize = true;
+            this.lblExperiencia.Location = new System.Drawing.Point(140, 73);
+            this.lblExperiencia.Name = "lblExperiencia";
+            this.lblExperiencia.Size = new System.Drawing.Size(46, 17);
+            this.lblExperiencia.TabIndex = 7;
+            this.lblExperiencia.Text = "label5";
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Location = new System.Drawing.Point(140, 99);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(46, 17);
+            this.lblNivel.TabIndex = 6;
+            this.lblNivel.Text = "label6";
+            // 
+            // lblOro
+            // 
+            this.lblOro.AutoSize = true;
+            this.lblOro.Location = new System.Drawing.Point(140, 45);
+            this.lblOro.Name = "lblOro";
+            this.lblOro.Size = new System.Drawing.Size(46, 17);
+            this.lblOro.TabIndex = 5;
+            this.lblOro.Text = "label7";
+            // 
+            // lblPuntosDeImpacto
+            // 
+            this.lblPuntosDeImpacto.AutoSize = true;
+            this.lblPuntosDeImpacto.Location = new System.Drawing.Point(140, 19);
+            this.lblPuntosDeImpacto.Name = "lblPuntosDeImpacto";
+            this.lblPuntosDeImpacto.Size = new System.Drawing.Size(46, 17);
+            this.lblPuntosDeImpacto.TabIndex = 4;
+            this.lblPuntosDeImpacto.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 643);
+            this.Controls.Add(this.lblExperiencia);
+            this.Controls.Add(this.lblNivel);
+            this.Controls.Add(this.lblOro);
+            this.Controls.Add(this.lblPuntosDeImpacto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mi Juego";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblExperiencia;
+        private System.Windows.Forms.Label lblNivel;
+        private System.Windows.Forms.Label lblOro;
+        private System.Windows.Forms.Label lblPuntosDeImpacto;
+    }
+}
+
---
+++ MiJuego.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPG
+{
+    public partial class MiJuego : Form
+    {
+        private Player _player;
+
+
+        //Construstor
+        public MiJuego()
+        {
+            InitializeComponent();
+
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            lblPuntosDeImpacto.Text = _player.CurrentHitPoints.ToString();
+            lblOro.Text = _player.Gold.ToString();
+            lblExperiencia.Text = _player.ExperiencePoints.ToString();
+            lblNivel.Text = _player.Level.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
